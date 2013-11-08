90fc1f7957a847a0ef9e8fb6fc14e9a3e4c4129f
Marco <marcos1490@gmail.com>
2013-11-08 13:42:12 -0600

    Write text on canvas as user writes on input.

--- functions.js
+++ functions.js
@@ -1,24 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$("#sendString").click(function(){
+	$("#sendString").click(function() {
 		var inputStringText = $("#inputString").val();
 		var fontSize = $("#fontSize").val();
 
 		updateCanvas(inputStringText, fontSize);
 	});
 
-	$("#fontSize").change(function(){
+	$("#fontSize").change(function() {
 		var inputStringText = $("#inputString").val();
 		var fontSize = $("#fontSize").val();
-		updateCanvas(inputStringText, fontSize)
+		updateCanvas(inputStringText, fontSize);
+	});
+	$("#inputString").keyup(function(event) {
+		var inputStringText = this.value;
+		var fontSize = $("#fontSize").val();
+
+		updateCanvas(inputStringText, fontSize);
 	});
 
-	function updateCanvas(inputStringText, fontSize){
-		$('.myCanvas').customText('update', { 'text' : inputStringText, fontSize : fontSize+'px'});		
+	function updateCanvas(inputStringText, fontSize) {
+		$('.myCanvas').customText('update', {
+			'text': inputStringText,
+			fontSize: fontSize + 'px'
+		});
 	}
 
 
-
 	$('.myCanvas').customText('init');
-		
+
 });
